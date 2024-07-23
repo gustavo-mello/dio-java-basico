3c16c33d7078cdfce4ff00ede6f2c5d1c785edb7
Gustavo Mello <gugamello40@gmail.com>
2024-07-23 15:57:40 -0300

    Completando o desafio de Padrões de Projetos

---
+++ padroes-projeto-spring/src/main/java/padroes_projeto_spring/PadroesProjetoSpringApplication.java
@@ -0,0 +1,16 @@
+package padroes_projeto_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication(scanBasePackages = "padroes_projeto_spring")
+@EnableFeignClients
+
+public class PadroesProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadroesProjetoSpringApplication.class, args);
+	}
+
+}
